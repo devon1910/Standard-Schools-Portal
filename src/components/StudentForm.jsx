60588093c585f7290fde1f7903f08c5c020d6b68
devon1910 <ekpodave2000@gmail.com>
2025-08-28 18:29:24 +0100

    Updated Button name to 'Save Changes' from 'Add Student'

--- src/components/StudentForm.jsx
+++ src/components/StudentForm.jsx
@@ -416,7 +416,7 @@ const StudentForm = ({ onSubmit, classId, selectedSession, initialData, selected
           type="submit"
           className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-primary-orange hover:bg-opacity-80 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-orange transition-colors cursor-pointer"
         >
-          {initialData ? 'Update Student' : 'Add Student'}
+          {initialData ? 'Update Changes' : 'Save Changes'}
         </button>
       </div>
     </form>
